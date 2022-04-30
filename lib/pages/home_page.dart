da81407e52cf30e9c267e6621348963ca986c891
zaenalashter <santrialit77@gmail.com>
2022-04-30 22:45:57 +0700

    chatty 4

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:bwa_chatty/pages/theme.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -6,8 +7,40 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Image.asset(
-        '/images/profile_pic.png',
+      backgroundColor: blueColor,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 40,
+              ),
+              Image.asset(
+                '/images/profile_pic.png',
+                height: 100,
+                width: 100,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Sabrina Carpenter',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: whiteColor,
+                ),
+              ),
+              SizedBox(
+                height: 2,
+              ),
+              Text('Travel Freelancer',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: lightBlueColor,
+                  )),
+            ],
+          ),
+        ),
       ),
     );
   }
