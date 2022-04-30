53ea6311b8f7d248432110f5c0370bdda46d1755
zaenalashter <santrialit77@gmail.com>
2022-04-30 23:51:26 +0700

    chatty 8

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,73 +11,103 @@ class HomePage extends StatelessWidget {
       backgroundColor: blueColor,
       body: SafeArea(
         child: Center(
-          child: Column(
-            children: [
-              SizedBox(
-                height: 40,
-              ),
-              Image.asset(
-                '/images/profile_pic.png',
-                height: 100,
-                width: 100,
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Text(
-                'Sabrina Carpenter',
-                style: TextStyle(
-                  fontSize: 20,
-                  color: whiteColor,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 40,
                 ),
-              ),
-              SizedBox(
-                height: 2,
-              ),
-              Text(
-                'Travel Freelancer',
-                style: TextStyle(
-                  fontSize: 16,
-                  color: lightBlueColor,
+                Image.asset(
+                  '/images/profile_pic.png',
+                  height: 100,
+                  width: 100,
                 ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Container(
-                width: double.infinity,
-                padding: EdgeInsets.all(30),
-                decoration: BoxDecoration(
-                  color: whiteColor,
-                  borderRadius: BorderRadius.vertical(
-                    top: Radius.circular(40),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  'Sabrina Carpenter',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: whiteColor,
                   ),
                 ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Friends',
-                      style: tittleTextStyle,
-                    ),
-                    ChatTile(
-                      '/images/friend1.png',
-                      'Joshuer',
-                      'Sorry, you’re not my ty...',
-                      'Now',
-                      true,
-                    ),
-                    ChatTile(
-                      '/images/friend2.png',
-                      'Gabriella',
-                      'I saw it clearly and mig...',
-                      '2:30',
-                      false,
-                    ),
-                  ],
+                SizedBox(
+                  height: 2,
                 ),
-              ),
-            ],
+                Text(
+                  'Travel Freelancer',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: lightBlueColor,
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.all(30),
+                  decoration: BoxDecoration(
+                    color: whiteColor,
+                    borderRadius: BorderRadius.vertical(
+                      top: Radius.circular(40),
+                    ),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Friends',
+                        style: tittleTextStyle,
+                      ),
+                      ChatTile(
+                        '/images/friend1.png',
+                        'Joshuer',
+                        'Sorry, you’re not my ty...',
+                        'Now',
+                        true,
+                      ),
+                      ChatTile(
+                        '/images/friend2.png',
+                        'Gabriella',
+                        'I saw it clearly and mig...',
+                        '2:30',
+                        false,
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Text(
+                        'Groups',
+                        style: tittleTextStyle,
+                      ),
+                      ChatTile(
+                        '/images/group1.png',
+                        'Jakarta Fair',
+                        'Why does everyone ca...',
+                        '11:11',
+                        false,
+                      ),
+                      ChatTile(
+                        '/images/group2.png',
+                        'Angga',
+                        'Why does everyone ca...',
+                        '7:11',
+                        true,
+                      ),
+                      ChatTile(
+                        '/images/group3.png',
+                        'Bentley',
+                        'Why does everyone ca...',
+                        '6:11',
+                        true,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
