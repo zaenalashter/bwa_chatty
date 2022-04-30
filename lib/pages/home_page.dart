008b78c57a6d26d4042653ee9ca85ba6dd5a7bf8
zaenalashter <Santrialit77@gmail.com>
2022-04-30 14:35:31 +0700

    chatty 2

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,10 +6,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text(
-        'Home Page',
-        style: TextStyle(fontSize: 40),
-      ),
+      body: Image.asset('/images/profile_pic.png',),
     );
   }
 }
